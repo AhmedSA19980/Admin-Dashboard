54d5fa7b85b36cfb324f52125c9bf9ca7c296362
miche <AhmedSA19980>
2026-03-06 01:37:00 +0300

    add Refresh_token rep

--- backend/Admin.Core/interfaces/refresh_token/IRefreshToken.cs
+++ backend/Admin.Core/interfaces/refresh_token/IRefreshToken.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Admin.Core.DTOs.token;
 
 namespace  Admin.Core.interfaces.refresh_token
 {
-    public  interface IRefreshToken<T> : IWriteRepository<T> where T : class
+    public  interface IRefreshToken<T> : IWriteRepository<T>  where T : class
     {
+
+        Task<T> GetRefreshTokenByTokenAsync(string refreshToken);
+        Task<bool> RefreshTokenAsync (ReqRefreshTokenDTO Token);
+        Task<bool> RevokeAllRefreshTokensForUserAsync(RevokeAllRefreshTokensForUserDTO Token);
     }
 }
