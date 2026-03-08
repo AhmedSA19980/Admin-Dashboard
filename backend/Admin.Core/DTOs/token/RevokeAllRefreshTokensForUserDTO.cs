a01868013520b7ff41f96c5e171424a33870007e
miche <AhmedSA19980>
2026-03-09 00:50:54 +0300

    add =>RevokeAllRefreshTokensForUser.file

---
+++ backend/Admin.Core/DTOs/token/RevokeAllRefreshTokensForUserDTO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.DTOs.token
+{
+    public class RevokeAllRefreshTokensForUserDTO
+    {
+      public int userId { set; get; }
+      public string revokedByIp { set; get; }
+        public DateTime Revoked { get; set; } 
+        public string? ReasonRevoked {  set; get; }
+
+
+    }
+}
