4ec5116f9f21e699043a6dece5fa2209bdadb398
miche <AhmedSA19980>
2026-03-28 02:29:51 +0300

    user role dto

---
+++ backend/Admin.Core/DTOs/UserRoles/UserRoleDto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.DTOs.UserRoles
+{
+    public class UserRoleDto
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int RoleId { get; set; }
+
+        public string RoleName { get; set; }
+        public string UserName {  get; set; }
+        
+
+     
+    }
+}
