7717a3bf681d578553ab65290204d5553ef3661a
miche <AhmedSA19980>
2026-03-28 02:26:53 +0300

    user mapper

---
+++ backend/Admin.Application/Mappers/UserRoleMapper.cs
@@ -0,0 +1,27 @@
+﻿using Admin.Core.DTOs.UserRoles;
+using Admin.Core.models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Application.Mappers
+{
+    public static  class UserRoleMapper
+    {
+
+        public static  UserRoleDto ToDto(this UserRole UserRole)
+        {
+            return new UserRoleDto
+            {
+                Id = UserRole.Id,
+                RoleId = UserRole.Id,
+                UserId = UserRole.UserId,
+                RoleName = UserRole.Role.Name,
+                UserName = UserRole.User.UserName,   
+            };
+
+        }
+    }
+}
