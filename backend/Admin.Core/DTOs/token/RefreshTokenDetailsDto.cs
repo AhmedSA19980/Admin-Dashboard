291c4adb1c1b1d8e169d28aa32f1f95fec2ad543
miche <AhmedSA19980>
2026-04-16 01:29:29 +0300

    refresh details dto props are added

--- backend/Admin.Core/DTOs/token/RefreshTokenDetailsDto.cs
+++ backend/Admin.Core/DTOs/token/RefreshTokenDetailsDto.cs
@@ -1,12 +1,34 @@
-﻿using System;
+﻿using Admin.Core.models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Admin.Core.DTOs.token
 {
-    internal class RefreshTokenDetailsDto
+    public  class RefreshTokenDetailsDto
     {
+       
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public string RefreshTok { get; set; } = null!;
+        public DateTime CreatedAt { get;  set; }
+
+        public DateTime ExpiresAt { get; set; }
+
+        public DateTime? Revoked { get; set; }
+        public string? RevokedByIP { get; set; }
+        public string? ReplaceByToken { get; set; }
+
+        public string? ReasonRevoked { get; set; }
+        public bool IsExpired { get;  }
+        public bool IsRevoked { get;  }
+        public bool IsActive { get; }
+
+
     }
 }
