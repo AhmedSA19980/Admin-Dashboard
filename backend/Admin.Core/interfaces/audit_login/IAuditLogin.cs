296334e5e00dd942864d0bbfc51e432af9ecc9c9
miche <AhmedSA19980>
2026-06-28 02:20:52 +0300

    Adding Method

--- backend/Admin.Core/interfaces/audit_login/IAuditLogin.cs
+++ backend/Admin.Core/interfaces/audit_login/IAuditLogin.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Admin.Core.models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,7 @@ namespace  Admin.Core.interfaces.audit_login
 {
     public interface IAuditLogin<T> : IWriteRepository<T> , IReadRepository<T> where T : class
     {
-
+        public  Task<List<T>> GetAuditLogsAsync();
+        public Task<List<T>> getAuditLogsBetweenSpecificDateAsync(DateTime stDate, DateTime endate);
     }
 }
