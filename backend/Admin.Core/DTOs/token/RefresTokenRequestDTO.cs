1ae22f28b29c64c2dd0e0ac11f98544f059883f3
miche <AhmedSA19980>
2026-07-02 14:53:59 +0300

    Add refreshtoken request

---
+++ backend/Admin.Core/DTOs/token/RefresTokenRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.DTOs.token
+{
+    public class RefresTokenRequestDTO
+    {
+       
+        public string RefreshToken { get; set; }
+
+        public string Email { get; set; }
+
+    }
+}
