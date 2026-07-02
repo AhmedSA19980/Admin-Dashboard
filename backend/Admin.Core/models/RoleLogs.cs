9c7cf7dd250e0a496141e82f3304e684e2f75e22
miche <AhmedSA19980>
2026-07-02 14:57:02 +0300

    Add role logs models

---
+++ backend/Admin.Core/models/RoleLogs.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Admin.Core.models
+{
+    public  class RoleLogs
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int AdminId { get; set; }
+
+        public int UserId { get; set; }
+
+        public int RoleId {  get; set; }
+
+        public DateTimeOffset RoleAssignedDate { get; set; }
+
+        public string? Report {  get; set; }
+
+
+    }
+}
