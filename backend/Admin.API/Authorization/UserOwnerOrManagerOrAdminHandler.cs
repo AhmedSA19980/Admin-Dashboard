9de26b5c29416d341c407b764e8ca5c71cd53689
miche <AhmedSA19980>
2026-07-02 14:23:14 +0300

    Add auth Handller & requirment

---
+++ backend/Admin.API/Authorization/UserOwnerOrManagerOrAdminHandler.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Security.Claims;
+
+namespace Admin.API.Authorization
+{
+    public class UserOwnerOrManagerOrAdminHandler:AuthorizationHandler<UserOwnerOrManagerOrAdminRequirment, int>
+    {
+        protected override Task HandleRequirementAsync(
+       AuthorizationHandlerContext context,
+       UserOwnerOrManagerOrAdminRequirment requirement,
+       int studentId)
+        {
+            // Admin override
+            if (context.User.IsInRole("Admin"))
+            {
+                context.Succeed(requirement);
+                return Task.CompletedTask;
+            }
+
+            // Ownership check
+            var userId = context.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (int.TryParse(userId, out int authenticatedStudentId) &&
+                authenticatedStudentId == studentId)
+            {
+                context.Succeed(requirement);
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
