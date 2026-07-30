f2fc431166532895211c07ba5688d5966d7181e1
miche <AhmedSA19980>
2026-07-30 13:48:58 +0300

    change main page + add hero section

--- frontend/admin-dashboard/src/app/page.tsx
+++ frontend/admin-dashboard/src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
-import { HeroSection } from "@/components/layout/HeroSection";
+import { HeroSection } from "@/components/layout/herosection";
 import Image from "next/image";
+
+
 //flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black
 //*lex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start
 export default function Home() {
