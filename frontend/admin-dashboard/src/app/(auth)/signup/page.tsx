84b5ebe71607703aedc7ddf24d2733d3bf25e7af
miche <AhmedSA19980>
2026-07-30 13:53:51 +0300

    set background for both page 1-signup 2-login

--- frontend/admin-dashboard/src/app/(auth)/signup/page.tsx
+++ frontend/admin-dashboard/src/app/(auth)/signup/page.tsx
@@ -2,9 +2,33 @@ import SignUpForm from "@/features/auth/signupform";
 
 export default function SignUp (){
     return (
-      <div className="bg">
+      <div className="relative isolate px-6 pt-14 lg:px-8 bg-black">
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-40 -top-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+        >
+          <div
+            style={{
+              clipPath:
+                "clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+            }}
+            className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff000] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+          ></div>
+        </div>
         <div className="flex min-h-screen items-center justify-center shadow-lg">
           <SignUpForm></SignUpForm>
+        </div>
+        <div
+          aria-hidden="true"
+          className="absolute inset-x-0 -bottom-0 -z-10 transform-gpu overflow-hidden blur-3xl sm:-bottom-0"
+        >
+          <div
+            style={{
+              clipPath:
+                "clip-path: polygon(75.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%))",
+            }}
+            className="relative left-[calc(40%-5rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff000] to-[#9089fc] opacity-30 sm:left-[calc(50%-15rem)] sm:w-350.75"
+          ></div>
         </div>
       </div>
     );
